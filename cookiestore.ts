2b0c2559009ab6ccb3d8705d4808a30b21831d9a
Johannes Schickling <schickling.j@gmail.com>
2016-09-13 17:03:18 +0200

    updated type definitions

---
+++ cookiestore.ts
@@ -0,0 +1,39 @@
+import * as cookies from 'js-cookie'
+
+const domain = extractDomain()
+
+export type Key = 'graphcool_auth_token'
+  | 'graphcool_customer_id'
+  | 'graphcool_last_used_project_id'
+
+function extractDomain (): string {
+  const hostname = window.location.hostname
+
+  if (hostname.match(/^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|$)){4}$/)) {
+    return hostname
+  } else {
+    return hostname.split('.').slice(-2).join('.')
+  }
+}
+
+export function set (key: Key, value: string): void {
+  cookies.set(key, value, {
+    domain,
+    expires: 90,
+  })
+}
+
+export function has (key: Key): boolean {
+  return cookies.get(key) !== undefined
+}
+
+export function get (key: Key): string {
+  if (!has(key)) {
+    throw new Error(`No value for key: ${key}`)
+  }
+  return cookies.get(key)
+}
+
+export function remove (key: Key): void {
+  cookies.remove(key, { domain })
+}
